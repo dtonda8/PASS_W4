9b3e5ac69e926cfde9e0ed1afcbe6f4179e608a7
dbat0014 <dbat0014@student.monash.edu>
2024-03-20 14:41:53 +1100

    Add solutions

--- Q1.py
+++ Q1.py
@@ -1,8 +1,30 @@
 from typing import List, TypeVar
+from data_structures.array_list import ArrayList
 T = TypeVar("T")
 
 def merge(list1: List[T], list2: List[T]) -> List[T]:
-	raise NotImplementedError()
+	p1 = 0
+	p2 = 0
+	n1 = len(list1)
+	n2 = len(list2)
+	ans = ArrayList(n1 + n2)
 
-if __name__ == "__main__": # for personal tests
-	pass
+	while p1 < n1 and p2 < n2:
+		if list1[p1] < list2[p2]:
+			ans.append(list1[p1])
+			p1 += 1
+		else:
+			ans.append(list2[p2])
+			p2 += 1
+	
+	# Add leftover
+	for i in range(p1, n1):
+		ans.append(list1[i])
+	
+	for i in range(p2, n2):
+		ans.append(list2[i])
+
+	return ans
+
+# if __name__ == "__main__": # for personal tests
+# 	pass
--- Q2.py
+++ Q2.py
@@ -1,8 +1,39 @@
 from typing import List, TypeVar
+from data_structures.queue_adt import CircularQueue
 T = TypeVar("T")
 
 def time_required_to_buy(tickets: List[T], k: int) -> int:
-	raise NotImplementedError()
+	## Using Lists
+	# ans = 0
+	# while tickets[k] > 0:
+	# 	for i in range(len(tickets)):
+	# 		if tickets[i] == 0:
+	# 			continue
+	# 		tickets[i] -= 1
+	# 		ans += 1
+	# 		if tickets[k] == 0:
+	# 			return ans
+
+	## Using Circular Queue
+	# q = CircularQueue(len(tickets))
+	# ans = 0
+	
+	# # populate queue
+	# for i in range(len(tickets)):
+	# 	q.append(tickets[i])
+
+	# while True:
+	# 	for i in range(len(tickets)):
+	# 		nxt = q.serve()
+	# 		if nxt > 0:
+	# 			nxt -= 1
+	# 			ans += 1
+	# 		if i == k and nxt == 0:
+	# 			return ans
+	# 		q.append(nxt)
+	pass
+	
+
 
 if __name__ == "__main__": # for personal tests
 	pass
